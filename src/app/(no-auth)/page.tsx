253968bb43e1e52ed2c2f6217f89cbacd6bac758
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-09-04 15:24:24 -0300

    chore: initial setup and login page for environment mobile

---
+++ src/app/(no-auth)/page.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { LoaderCircleIcon } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  email: z
+    .string({
+      message: 'Campo obrigatório',
+    })
+    .email({
+      message: 'E-mail inválido',
+    }),
+  password: z
+    .string({
+      message: 'Campo obrigatório',
+    })
+    .min(8, {
+      message: 'Mínimo de 8 caracteres',
+    }),
+})
+
+type FormValues = z.infer<typeof formSchema>
+
+export default function Auth() {
+  const [isLoadingAuth, setIsLoadingAuth] = useState<boolean>(true)
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const handleSubmit = form.handleSubmit((data: FormValues) => {
+    setIsLoadingAuth(true)
+    console.log(data)
+    setIsLoadingAuth(false)
+  })
+
+  return (
+    <div className="flex h-full items-center justify-center bg-primary px-4">
+      <div className="w-full rounded-md bg-white px-2 py-3">
+        <h1 className="mb-3 text-center text-3xl font-bold">
+          trainer<span className="text-primary">.dev</span>
+        </h1>
+        <Form {...form}>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Ex: johnDoe@smn.com.br"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Ex: 123456"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="mt-2 w-full">
+              {isLoadingAuth ? (
+                <LoaderCircleIcon className="animate-spin" />
+              ) : (
+                'Entrar'
+              )}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  )
+}
